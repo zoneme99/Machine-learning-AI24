2d41e03ee5329e4142de186c71c4b8be69dcefcb
Jonatan Cervall <jonatancervall@gmail.com>
2025-03-28 13:33:42 +0100

    nästa steg är att fylla alla filmer utan någon tag med nollor i tfidf matrisen

--- Labs/Film/script.py
+++ Labs/Film/script.py
@@ -1,25 +1,54 @@
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from functools import cache
+import numpy as np
+from scipy.sparse import csr_matrix, hstack
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
 
 @cache
-def create_df():
-    movies = pd.read_csv('Labs/Film/ml-latest/movies.csv')
-    df_one_hot = movies['genres'].str.get_dummies(sep='|')
-    movies = movies.drop(columns='genres')
-    df = pd.concat([movies,df_one_hot], axis=1)
-    
-    tags = pd.read_csv('Labs/Film/ml-latest/tags.csv')
-    print(tags['tag'].shape)
-    tags = tags.dropna()
-    vector = TfidfVectorizer()
-    tag_vectorized = vector.fit_transform(tags['tag'])
+def create_matrix():
 
-    #tfidf_df = pd.DataFrame(tag_vectorized.toarray(), columns=vector.get_feature_names_out())
-    #print(tfidf_df)
-    return tag_vectorized
+    def refine_movies_ratings():
+        #Movie refinement
+        movies = pd.read_csv('Labs/Film/ml-latest/movies.csv', index_col='movieId')
+        df_one_hot = movies['genres'].str.get_dummies(sep='|')
+        movies = movies.drop(columns=['genres','title'])
+        movies = pd.concat([movies,df_one_hot], axis=1)
+        ratings = pd.read_csv('Labs/Film/ml-latest/ratings.csv')
+        #Rating refinement
+        mask = ratings.groupby('userId').size() > 25
+        expert_ratings = ratings[ratings['userId'].isin(mask[mask].index)] # Sorterar bort ej "expert users"
+        refined_ratings = expert_ratings.groupby('movieId')['rating'].mean() #Minskar datan genom att ta ett medel för varje film
+        movies = movies[movies.index.isin(refined_ratings.index)] # Filtrerar bort alla filmer som inte finns med hos expert users
+        S_scaler = StandardScaler()
+        rate_array = refined_ratings.array.reshape((-1, 1))
+        scaled_rating = S_scaler.fit_transform(rate_array)
+        min_scaler = MinMaxScaler()
+        scaled_rating = min_scaler.fit_transform(scaled_rating)
+        #Konkatinera
+        movies = csr_matrix(movies.values)
+        sparse = hstack([movies, scaled_rating])
+        return sparse
+
+    
+    def refine_tags():
+        tags = pd.read_csv('Labs/Film/ml-latest/tags.csv')
+        tags = tags.drop(columns=['userId', 'timestamp'])
+        tags = tags.dropna()
+        tags['tag'] = tags['tag'].apply(lambda x: str(x))
+        grouped_tags = tags.groupby('movieId')['tag'].agg(' '.join)
+        vector = TfidfVectorizer()
+        tag_vectorized = vector.fit_transform(grouped_tags)
+        return tag_vectorized
+    
+    part1 = refine_movies_ratings()
+    part2 = refine_tags()
+    print(part1.shape, part2.shape)
+    exit()
+    return hstack([part1, part2])
+    
 #def model_selection(df):
 
 
-df = create_df()
-print(df.data)
+matrix = create_matrix()
+print(matrix)
