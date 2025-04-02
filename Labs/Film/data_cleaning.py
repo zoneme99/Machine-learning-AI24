453fe31ddb640897b2c87060f38a0b8f1c74b5db
Jonatan Cervall <jonatancervall@gmail.com>
2025-04-02 12:50:42 +0200

    Behöver skala ratings och genre eller låta tags vara enskilt

--- Labs/Film/data_cleaning.py
+++ Labs/Film/data_cleaning.py
@@ -26,21 +26,33 @@ def create_matrix():
         scaled_rating = S_scaler.fit_transform(rate_array)
         min_scaler = MinMaxScaler()
         scaled_rating = min_scaler.fit_transform(scaled_rating)
+        indices = movies.index # Används för att filtrera filmer med tags
         #Konkatinera
         movies = csr_matrix(movies.values)
-        return movies, scaled_rating, refined_ratings.index
+        count = 0
+        for x in indices:
+            if x == 260:
+                break
+            count += 1
+        print(count)
+        print(movies.getrow(count))
+        
+        exit()
+        return movies, scaled_rating, indices
 
     
     def refine_tags(ratings):
         tags = pd.read_csv('Labs/Film/ml-latest/tags.csv')
         tags = tags.drop(columns=['userId', 'timestamp'])
         tags = tags.dropna()
+        tags = tags.drop_duplicates('tag')
         tags['tag'] = tags['tag'].apply(lambda x: str(x))
         grouped_tags = tags.groupby('movieId')['tag'].agg(' '.join)
         tags_ratings = grouped_tags[grouped_tags.index.isin(ratings)] # Filmer med tags och ratings
         no_tag = ratings.difference(tags_ratings.index) # Filmer utan några taggar
         for index in no_tag:
             tags_ratings.loc[index] = '' #Fyller alla filmer med inga taggar till tomma strings, detta för att få lika stor mängd datapunkter i movies och tags (Dock lite beräkningstung, men ska bara köras en gång)
+        tags_ratings.sort_index(inplace=True)
         y = pd.Series(tags_ratings.index)
         vector = TfidfVectorizer()
         tag_vectorized = vector.fit_transform(tags_ratings)
--- Labs/Film/test.py
+++ Labs/Film/test.py
@@ -2,19 +2,22 @@ import pandas as pd
 import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
-from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.metrics.pairwise import cosine_similarity, euclidean_distances
 
+tags = pd.read_csv('Labs/Film/ml-latest/tags.csv')
 class_index = pd.read_csv('Labs/Film/class_index.csv', index_col=0)
 links = pd.read_csv('Labs/Film/ml-latest/links.csv', index_col='movieId')
 matrix = sp.sparse.load_npz('Labs/Film/matrix.npz')
+movies = pd.read_csv('Labs/Film/ml-latest/movies.csv')
 # Exempel-DataFrame
 def retrieve_recommendations(class_index, links, matrix):
 
 
-    serie = pd.Series(cosine_similarity(matrix, matrix.getrow(248)).reshape(-1))
-    serie = serie.sort_values(ascending=False)
+    serie = pd.Series(cosine_similarity(matrix, matrix.getrow(257)).reshape(-1))
+    serie = serie.sort_values(ascending=True)
     recommended_movies = serie.iloc[1:6]
-
+    print(matrix.getrow(recommended_movies.index[0]))
+    exit()
     for id in recommended_movies.index:
         urlnum = str(*links['imdbId'].loc[class_index.loc[id]].values)
         if len(urlnum) != 7:
@@ -22,6 +25,6 @@ def retrieve_recommendations(class_index, links, matrix):
             urlnum = diff*'0' + urlnum
         print(f'https://www.imdb.com/title/tt{urlnum}')
 
-#retrieve_recommendations()
+retrieve_recommendations(class_index, links, matrix)
 
-print(matrix.getrow(0))
+#257 new hope, 1166 empire strikes back
